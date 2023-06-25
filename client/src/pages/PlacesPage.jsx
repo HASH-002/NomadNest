0d7bd762da4e5ed02ac7d3b839ca8c2df12176ee
HASH-002 <koserhasan002@gmail.com>
2023-06-25 19:21:02 +0530

    Minor CSS change

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -32,7 +32,7 @@ export default function PlacesPage() {
 
             <div className="mt-4">
                 {places.length > 0 && places.map(place => (
-                    <Link key={place._id} to={'/account/places/' + place._id} className="flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl">
+                    <Link key={place._id} to={'/account/places/' + place._id} className="flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl mb-3">
                         <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
                             <PlaceImg place={place} />
                         </div>
