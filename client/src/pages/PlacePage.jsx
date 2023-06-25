3ae3ee84947f4dd3a62b919522294a849f44b1d8
HASH-002 <koserhasan002@gmail.com>
2023-06-25 16:16:00 +0530

    adding address component

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import PlaceGallery from "../PlaceGallery";
 import BookingWidget from "../BookingWidget";
+import AddressLink from "../AddressLink";
 
 export default function PlacePage() {
 
@@ -21,15 +22,8 @@ export default function PlacePage() {
     return (
         <div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8">
             <h1 className="text-3xl">{place.title}</h1>
-            <a href={"http://maps.google.com/?q=" + place.address} target="_blank" rel="noopener noreferrer"
-                className="flex gap-1 font-semibold underline">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-                </svg>
-                {place.address}</a>
 
-            {/* <AddressLink>{place.address}</AddressLink> */}
+            <AddressLink>{place.address}</AddressLink>
 
             <PlaceGallery place={place} />
 
